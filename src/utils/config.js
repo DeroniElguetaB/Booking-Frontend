38245059fa393fe7972dd2554cf2debd335c6764
Deroni Elgueta <deroniaeb@gmail.com>
2024-03-08 16:38:49 -0300

    cambios npm front

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
-// export const BASE_URL = 'http://localhost:4000/api/v1'
-// export const BASE_URL = 'http://mern-travel-booking-api.vercel.app/'
-export const BASE_URL = 'https://booking-backend-10tk.onrender.com/'
+//export const BASE_URL = 'http://localhost:4000/api/v1'
+export const BASE_URL = 'http://mern-travel-booking-api.vercel.app/'
+//export const BASE_URL = 'https://booking-backend-10tk.onrender.com/'
