e5015cfeb2222166f46971a1a40f24b738f3a211
Deroni Elgueta <deroniaeb@gmail.com>
2024-03-08 17:37:02 -0300

    detalles finales

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,5 @@
 //export const BASE_URL = 'http://localhost:4000/api/v1'
 //export const BASE_URL = 'http://mern-travel-booking-api.vercel.app/'
 export const BASE_URL = 'https://booking-backend-w23y.onrender.com/api/v1'
+
+//agregar al final del link al back en este caso '/api/v1' para traer bien el backend.
